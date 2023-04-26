1a70fa9a6fcf68dec20743281eac449c525bd4c9
Tyler Coffey <tyler.coffey@wsu.edu>
2023-04-25 19:57:24 -0700

    Update User.js
    Reformatting and removing some unnecessary comments

--- models/User.js
+++ models/User.js
@@ -10,41 +10,15 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    ClassList: {
-        type: mongoose.Types.ObjectId,
-        required: true
-    },
     isTeacher: {
         type: Boolean,
+        required: true
+    },
+    ClassList: {
+        type: mongoose.Types.ObjectId,
         required: true
     }
 });
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
-
-// class User {
-//     constructor(username, password) {
-//         this.username = username;
-//         this.password = password;
-//     }
-// }
-
-// class Student extends User {
-//     constructor(username, password) {
-//         super(username, password); // Calls the User constructor
-//     }
-
-// }
-
-// class Teacher extends User {
-//     constructor(username, password) {
-//         super(username, password); // Calls the User constructor
-//         this.Polls = Array(new Poll());
-//         this.Classes = Array(new ClassContainer(username));
-//     }
-
-//     addPoll(poll) {
-//         this.Polls.push(poll);
-//     }
-// }
